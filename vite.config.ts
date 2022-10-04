7a8a52ccc1b7bdeb299d6e83c684a7506df0160c
Itunu Lamina <Nix4phun@gmail.com>
2022-10-04 08:17:37 +0100

    chore: update customization

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,29 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-	plugins: [
-		react(),
-		dts({
-			insertTypesEntry: true,
-		}),
-	],
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, 'src/lib/index.ts'),
-			name: 'ReactFeatureFlag',
-			fileName: (format) => `use-toast-notification.${format}.js`,
-		},
-		rollupOptions: {
-			external: ['react'],
-		},
-	},
+export default defineConfig(({ command, mode, ssrBuild }) => {
+	if (command === 'build') {
+		return {
+			plugins: [
+				react(),
+				dts({
+					insertTypesEntry: true,
+				}),
+			],
+			build: {
+				lib: {
+					entry: path.resolve(__dirname, 'src/lib/index.ts'),
+					name: 'ReactFeatureFlag',
+					fileName: (format) => `use-toast-notification.${format}.js`,
+				},
+				rollupOptions: {
+					external: ['react'],
+				},
+			},
+		};
+	}
+
+	return {
+		plugins: [react()],
+	};
 });
